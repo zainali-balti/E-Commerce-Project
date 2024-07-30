93aed8c0fb301ac7986e16f8f241d61ad4b19bf3
zainali-balti <zainalicsc21s148@gmail.com>
2024-07-31 00:27:31 +0500

    E-Commerce Work

---
+++ Backend/Backend_E_Commerce/src/main/java/com/example/Backend_E_Commerce/BackendECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_E_Commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendECommerceApplication.class, args);
+	}
+
+}
---
+++ Backend/Backend_E_Commerce/src/test/java/com/example/Backend_E_Commerce/BackendECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_E_Commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
